6c3cac32062f2b031bb5403f5099f691178ef8c1
dongdong <donghyuk454@gmail.com>
2019-10-03 02:48:04 +0900

    1.0.1
    < 기능 추가

--- formscript.js
+++ formscript.js
@@ -8,6 +8,18 @@
     <input type="text" name="title" placeholder="제목을 입력하세요." style="width: 100%;"></input>
   </p>
   <strong>Description</strong><br>
-  <textarea name="descricption" placeholder="내용을 입력하세요." style="width: 100%; height: 300px;" onkeydown="if(event.keyCode===9){var v=this.value,s=this.selectionStart,e=this.selectionEnd;this.value=v.substring(0, s)+'\t'+v.substring(e);this.selectionStart=this.selectionEnd=s+1;return false;}"></textarea>
-  <input type="submit"></input>
+  <textarea name="descricption" id='desc' placeholder="내용을 입력하세요." style="width: 100%; height: 300px;" onkeydown="if(event.keyCode===9){var v=this.value,s=this.selectionStart,e=this.selectionEnd;this.value=v.substring(0, s)+'\t'+v.substring(e);this.selectionStart=this.selectionEnd=s+1;return false;}"></textarea>
+  <script>
+    function submit(){
+      setTimeout(function(){
+        document.getElementById('form').submit();
+      }, 500);
+    }
+    async function change(){
+      document.getElementById('desc').value = document.getElementById('desc').value.replace(/</gi, '&lt;');
+
+      await submit();
+    }
+  </script>
+  <input type="button" value="제출" onclick="change();"></input>
 </form>
